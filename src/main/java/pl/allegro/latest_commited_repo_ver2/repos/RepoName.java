34cdba45de6b21c6a0db7ebe31367a40063c8001
Paweł <lazar_obserwator@o2.pl>
2019-04-18 10:28:48 +0200

    refactoring to RepoName[]

--- src/main/java/pl/allegro/latest_commited_repo_ver2/repos/RepoName.java
+++ src/main/java/pl/allegro/latest_commited_repo_ver2/repos/RepoName.java
@@ -1,11 +1,13 @@
 package pl.allegro.latest_commited_repo_ver2.repos;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RepoName {
 
-    String repoName;
+    @JsonProperty("name")
+    private String repoName;
 
     public RepoName() {
     }
--- src/main/java/pl/allegro/latest_commited_repo_ver2/repos/ReposNames.java
+++ src/main/java/pl/allegro/latest_commited_repo_ver2/repos/ReposNames.java
@@ -2,22 +2,19 @@ package pl.allegro.latest_commited_repo_ver2.repos;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.List;
-
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ReposNames {
 
-    private List<RepoName> repoNameList;
+    private RepoName[] repoNameList;
 
     public ReposNames() {
     }
 
-    public List<RepoName> getRepoNameList() {
+    public RepoName[] getRepoNameList() {
         return repoNameList;
     }
 
-    public void setRepoNameList(List<RepoName> repoNameList) {
+    public void setRepoNameList(RepoName[] repoNameList) {
         this.repoNameList = repoNameList;
     }
 
