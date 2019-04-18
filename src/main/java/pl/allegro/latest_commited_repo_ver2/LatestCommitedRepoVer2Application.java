b6f01ef40d2c0d8060adf3f8f9205319053b9f57
Paweł <lazar_obserwator@o2.pl>
2019-04-18 06:17:28 +0200

    initial commit

---
+++ src/main/java/pl/allegro/latest_commited_repo_ver2/LatestCommitedRepoVer2Application.java
@@ -0,0 +1,13 @@
+package pl.allegro.latest_commited_repo_ver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatestCommitedRepoVer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatestCommitedRepoVer2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/allegro/latest_commited_repo_ver2/LatestCommitedRepoVer2ApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.allegro.latest_commited_repo_ver2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LatestCommitedRepoVer2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
