d6faf749c2d6c24bdf1325dcf4f3516ef7a0fcab
Paweł <lazar_obserwator@o2.pl>
2019-04-19 05:31:17 +0200

    refactored to List<LocalDateTime>

--- src/main/java/pl/allegro/latest_commited_repo_ver2/shas/ShasDate.java
+++ src/main/java/pl/allegro/latest_commited_repo_ver2/shas/ShasDate.java
@@ -1,20 +1,21 @@
 package pl.allegro.latest_commited_repo_ver2.shas;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
 public class ShasDate {
 
-    private Map<String, List<String>> repoNameWithLatestShasDateFromEachBranch;
+    private Map<String, List<LocalDateTime>> repoNameWithLatestShasDateFromEachBranch;
 
     public ShasDate() {
     }
 
-    public Map<String, List<String>> getRepoNameWithLatestShasDateFromEachBranch() {
+    public Map<String, List<LocalDateTime>> getRepoNameWithLatestShasDateFromEachBranch() {
         return repoNameWithLatestShasDateFromEachBranch;
     }
 
-    public void setRepoNameWithLatestShasDateFromEachBranch(Map<String, List<String>> repoNameWithLatestShasDateFromEachBranch) {
+    public void setRepoNameWithLatestShasDateFromEachBranch(Map<String, List<LocalDateTime>> repoNameWithLatestShasDateFromEachBranch) {
         this.repoNameWithLatestShasDateFromEachBranch = repoNameWithLatestShasDateFromEachBranch;
     }
 
